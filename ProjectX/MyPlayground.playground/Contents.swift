49ffdbc4c8a241c4e754eab8b7a2d6693708f23d
Madhukesh <madhukesh04@gmail.com>
2019-09-27 17:41:23 +0530

    swift basics-1

---
+++ ProjectX/MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+//This is comment
+var str:String = "Hello, playground"
+print(str)
+str = "some data"
+print(str)
+
+let constant = "This is IOS"
+
+var b:Bool = false
+print(b)
+b = true
+print(b)
+
+var i:Int = 10
+i = 0
+i = 12
+print(i)
+
+var f:Double = 0.188
