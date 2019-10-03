b7ad911ab0d36e374b3b15f721dfcbf59e208f88
Madhukesh <madhukesh04@gmail.com>
2019-10-03 10:43:37 +0530

    swift basics-2

--- ProjectX/MyPlayground.playground/Contents.swift
+++ ProjectX/MyPlayground.playground/Contents.swift
@@ -19,3 +19,27 @@ i = 12
 print(i)
 
 var f:Double = 0.188
+
+
+//Basic function
+func sayHello(){
+    print("Hello")
+}
+sayHello()
+
+//Function with parameters
+func sayHelloTo(_ name:String,_ age:Int){
+    print("hello \(name), you are \(age)")
+}
+
+sayHelloTo("Madhu",22)
+
+//Function with return value
+func addFourTo(x:Int) -> Int{
+    
+    let sum = x+4
+  return sum
+}
+
+var result = addFourTo(x: 20)
+print(result)
